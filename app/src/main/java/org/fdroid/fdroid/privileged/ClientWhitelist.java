0f318d036ce5fdb2fa03c9938cd36afc9ecf74b4
Rashed Abdel-Tawab <rashed@linux.com>
2018-11-05 13:05:55 -0800

    Use copperhead keys

--- app/src/main/java/org/fdroid/fdroid/privileged/ClientWhitelist.java
+++ app/src/main/java/org/fdroid/fdroid/privileged/ClientWhitelist.java
@@ -35,12 +35,12 @@ public class ClientWhitelist {
 
     public static HashSet<Pair<String, String>> whitelist = new HashSet<>(Arrays.asList(
             // certificate SHA-256 of https//f-droid.org/FDroid.apk
-            new Pair<>("org.fdroid.fdroid", "0"), // bullhead releasekey
-            new Pair<>("org.fdroid.fdroid", "0"), // angler releasekey
-            new Pair<>("org.fdroid.fdroid", "0"), // marlin releasekey
-            new Pair<>("org.fdroid.fdroid", "0"), // sailfish releasekey
-            new Pair<>("org.fdroid.fdroid", "0"), // taimen releasekey
-            new Pair<>("org.fdroid.fdroid", "0")  // walleye releasekey
+            new Pair<>("org.fdroid.fdroid", "A78DAF79F658BA3242ADE851637FEA184AF53DAF306C9131209FC5DC634A9396"), // bullhead releasekey
+            new Pair<>("org.fdroid.fdroid", "0E39376C3DFD4DE49EDEBD0E4629CF94B9C40C01D5B284B2112F2FF088FAD351"), // angler releasekey
+            new Pair<>("org.fdroid.fdroid", "6E461BDB712AC6A0CC3251B1DFEDD96C367E814116D9816D550E9DB606C1F628"), // marlin releasekey
+            new Pair<>("org.fdroid.fdroid", "2C679B2951D7D1D54DA1534AE8B7FF1681B195806E34985D040C9B9D6CB48874"), // sailfish releasekey
+            new Pair<>("org.fdroid.fdroid", "03F03912587E3DD68AB91F9165D35AFB04DF38BBFC38161C0D2592A6D385676F"), // taimen releasekey
+            new Pair<>("org.fdroid.fdroid", "B2D735F2B2BCA4DB548BBD606776AACD81AFE5430BC24C369D151D1AAF993D9F")  // walleye releasekey
     ));
 
 }
